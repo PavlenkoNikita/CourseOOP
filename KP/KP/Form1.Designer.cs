94882a676a709778a938c9e0ba5186587d026eda
Ilya <sg4wis@gmail.com>
2018-09-22 19:23:58 +0300

    Modified Main Form Design and Behaviour

--- KP/KP/Form1.Designer.cs
+++ KP/KP/Form1.Designer.cs
@@ -28,101 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_OutputResult = new System.Windows.Forms.Button();
+            this.rdBtn_Equilateral = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdBtn_Isosceles = new System.Windows.Forms.RadioButton();
+            this.rdBtn_Right = new System.Windows.Forms.RadioButton();
+            this.rdBtn_Arbitrary = new System.Windows.Forms.RadioButton();
+            this.txtBtn_A = new System.Windows.Forms.TextBox();
+            this.txtBtn_B = new System.Windows.Forms.TextBox();
+            this.txtBtn_Angle = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // btn_OutputResult
             // 
-            this.textBox1.Location = new System.Drawing.Point(62, 154);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
+            this.btn_OutputResult.Location = new System.Drawing.Point(205, 139);
+            this.btn_OutputResult.Name = "btn_OutputResult";
+            this.btn_OutputResult.Size = new System.Drawing.Size(185, 52);
+            this.btn_OutputResult.TabIndex = 3;
+            this.btn_OutputResult.Text = "Вычислить";
+            this.btn_OutputResult.UseVisualStyleBackColor = true;
+            this.btn_OutputResult.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox2
+            // rdBtn_Equilateral
             // 
-            this.textBox2.Location = new System.Drawing.Point(62, 202);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 1;
+            this.rdBtn_Equilateral.AutoSize = true;
+            this.rdBtn_Equilateral.Location = new System.Drawing.Point(16, 31);
+            this.rdBtn_Equilateral.Name = "rdBtn_Equilateral";
+            this.rdBtn_Equilateral.Size = new System.Drawing.Size(111, 21);
+            this.rdBtn_Equilateral.TabIndex = 4;
+            this.rdBtn_Equilateral.TabStop = true;
+            this.rdBtn_Equilateral.Text = "Правильный";
+            this.rdBtn_Equilateral.UseVisualStyleBackColor = true;
+            this.rdBtn_Equilateral.Click += new System.EventHandler(this.TriangleTypeChanged);
             // 
-            // textBox3
+            // groupBox1
             // 
-            this.textBox3.Location = new System.Drawing.Point(62, 250);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 2;
+            this.groupBox1.Controls.Add(this.rdBtn_Arbitrary);
+            this.groupBox1.Controls.Add(this.rdBtn_Right);
+            this.groupBox1.Controls.Add(this.rdBtn_Isosceles);
+            this.groupBox1.Controls.Add(this.rdBtn_Equilateral);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(185, 178);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Вид треугольника";
             // 
-            // button1
+            // rdBtn_Isosceles
             // 
-            this.button1.Location = new System.Drawing.Point(75, 298);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.rdBtn_Isosceles.AutoSize = true;
+            this.rdBtn_Isosceles.Location = new System.Drawing.Point(16, 68);
+            this.rdBtn_Isosceles.Name = "rdBtn_Isosceles";
+            this.rdBtn_Isosceles.Size = new System.Drawing.Size(143, 21);
+            this.rdBtn_Isosceles.TabIndex = 5;
+            this.rdBtn_Isosceles.TabStop = true;
+            this.rdBtn_Isosceles.Text = "Равнобедренный";
+            this.rdBtn_Isosceles.UseVisualStyleBackColor = true;
+            this.rdBtn_Isosceles.Click += new System.EventHandler(this.TriangleTypeChanged);
+            // 
+            // rdBtn_Right
+            // 
+            this.rdBtn_Right.AutoSize = true;
+            this.rdBtn_Right.Location = new System.Drawing.Point(16, 105);
+            this.rdBtn_Right.Name = "rdBtn_Right";
+            this.rdBtn_Right.Size = new System.Drawing.Size(133, 21);
+            this.rdBtn_Right.TabIndex = 6;
+            this.rdBtn_Right.TabStop = true;
+            this.rdBtn_Right.Text = "Прямоугольный";
+            this.rdBtn_Right.UseVisualStyleBackColor = true;
+            this.rdBtn_Right.Click += new System.EventHandler(this.TriangleTypeChanged);
+            // 
+            // rdBtn_Arbitrary
+            // 
+            this.rdBtn_Arbitrary.AutoSize = true;
+            this.rdBtn_Arbitrary.Location = new System.Drawing.Point(16, 142);
+            this.rdBtn_Arbitrary.Name = "rdBtn_Arbitrary";
+            this.rdBtn_Arbitrary.Size = new System.Drawing.Size(126, 21);
+            this.rdBtn_Arbitrary.TabIndex = 7;
+            this.rdBtn_Arbitrary.TabStop = true;
+            this.rdBtn_Arbitrary.Text = "Произвольный";
+            this.rdBtn_Arbitrary.UseVisualStyleBackColor = true;
+            this.rdBtn_Arbitrary.Click += new System.EventHandler(this.TriangleTypeChanged);
+            // 
+            // txtBtn_A
+            // 
+            this.txtBtn_A.Location = new System.Drawing.Point(102, 27);
+            this.txtBtn_A.Name = "txtBtn_A";
+            this.txtBtn_A.Size = new System.Drawing.Size(68, 22);
+            this.txtBtn_A.TabIndex = 6;
+            // 
+            // txtBtn_B
+            // 
+            this.txtBtn_B.Location = new System.Drawing.Point(102, 55);
+            this.txtBtn_B.Name = "txtBtn_B";
+            this.txtBtn_B.Size = new System.Drawing.Size(68, 22);
+            this.txtBtn_B.TabIndex = 7;
+            // 
+            // txtBtn_Angle
+            // 
+            this.txtBtn_Angle.Location = new System.Drawing.Point(102, 83);
+            this.txtBtn_Angle.Name = "txtBtn_Angle";
+            this.txtBtn_Angle.Size = new System.Drawing.Size(68, 22);
+            this.txtBtn_Angle.TabIndex = 8;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(302, 63);
+            this.label1.Location = new System.Drawing.Point(16, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Сторона a:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(302, 188);
+            this.label2.Location = new System.Drawing.Point(16, 58);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Сторона b:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(302, 304);
+            this.label3.Location = new System.Drawing.Point(54, 86);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Угол:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtBtn_B);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtBtn_A);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtBtn_Angle);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(205, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(185, 117);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ввод";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(406, 206);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_OutputResult);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_OutputResult;
+        private System.Windows.Forms.RadioButton rdBtn_Equilateral;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdBtn_Arbitrary;
+        private System.Windows.Forms.RadioButton rdBtn_Right;
+        private System.Windows.Forms.RadioButton rdBtn_Isosceles;
+        private System.Windows.Forms.TextBox txtBtn_A;
+        private System.Windows.Forms.TextBox txtBtn_B;
+        private System.Windows.Forms.TextBox txtBtn_Angle;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- KP/KP/Form1.cs
+++ KP/KP/Form1.cs
@@ -19,17 +19,29 @@ namespace KP
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double a = Convert.ToDouble(textBox1.Text);
-            double b = Convert.ToDouble(textBox2.Text);
-            double angle = Convert.ToDouble(textBox3.Text);
+            double a = Convert.ToDouble(txtBtn_A.Text);
+            double b = Convert.ToDouble(txtBtn_B.Text);
+            double angle = Convert.ToDouble(txtBtn_Angle.Text);
 
             Right_triangle right = new Right_triangle(a, b, angle);
             Isosceles_triangle isosceles = new Isosceles_triangle(a, b, angle);
             Equilateral_triangle equilateral = new Equilateral_triangle(a, b, angle);
 
-            label1.Text = $"Right:\na = {right.A}\nb = {right.B}\n angle = {right.Angle}\nPerimitr = {right.Perimetr()}\nSquare = {right.Square()}\n";
-            label2.Text = $"Isosceles:\na = {isosceles.A}\nb = {isosceles.B}\n angle = {isosceles.Angle}\nPerimitr = {isosceles.Perimetr()}\nSquare = {right.Square()}\n";
-            label3.Text = $"Equilateral:\na = {equilateral.A}\nb = {equilateral.B}\n angle = {equilateral.Angle}\nPerimitr = {equilateral.Perimetr()}\nSquare = {equilateral.Square()}\n";
+
+        }
+
+        private void TriangleTypeChanged(object sender, EventArgs e)
+        {
+            txtBtn_B.Enabled = true;
+            txtBtn_Angle.Enabled = true;
+
+            if (rdBtn_Equilateral.Checked)
+            {
+                txtBtn_B.Enabled = false;
+                txtBtn_Angle.Enabled = false;
+            }
+            else if (rdBtn_Isosceles.Checked) txtBtn_B.Enabled = false;
+            else if (rdBtn_Right.Checked) txtBtn_Angle.Enabled = false;
         }
     }
 }
