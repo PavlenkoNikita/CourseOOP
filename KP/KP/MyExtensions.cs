00e6c1f2436afd71a980713d4cd9e8242aa1c1e7
Ilya <sg4wis@gmail.com>
2018-09-29 14:36:47 +0300

    Added extension for angles

---
+++ KP/KP/MyExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KP
+{
+    public static class MyExtensions
+    {
+        public static double ToRadians(this double angle) => angle * Math.PI / 180;
+    }
+}
