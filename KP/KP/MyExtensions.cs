80bdd6848aebf688d9ab37df4dc1a3ae9f731806
Ilya <sg4wis@gmail.com>
2018-10-02 16:43:44 +0300

    New extension to get triangle info

--- KP/KP/MyExtensions.cs
+++ KP/KP/MyExtensions.cs
@@ -9,5 +9,16 @@ namespace KP
     public static class MyExtensions
     {
         public static double ToRadians(this double angle) => angle * Math.PI / 180;
+
+        public static string GetTriangleInfo(this Triangle triangle)
+        {
+            return string.Format($"Тип треугольника: {triangle.Type}\n\n" +
+                                 $"Сторона а: {triangle.A}\n" +
+                                 $"Сторона b: {triangle.B}\n" +
+                                 $"Сторона c: {triangle.C.ToString("#.##")}\n" +
+                                 $"Угол между a и b: {(triangle.Angle * 180 / Math.PI).ToString("#.##")}°\n\n" +
+                                 $"Периметр: {triangle.Perimetr().ToString("#.##")}\n" +
+                                 $"Площадь: {triangle.Square().ToString("#.##")}");
+        }
     }
 }
