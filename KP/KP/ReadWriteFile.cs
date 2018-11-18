e976416b7f3d684a3148fd118349c9e930647c86
Nikita Pavlenko <mapmejiad.ua@gmail.com>
2018-11-18 16:58:28 +0200

    Processing file triangles with invalid parameters

--- KP/KP/ReadWriteFile.cs
+++ KP/KP/ReadWriteFile.cs
@@ -2,6 +2,7 @@
 using System.Runtime.Serialization.Json;
 using System.IO;
 using System.Windows.Forms;
+using System;
 
 namespace KP
 {
@@ -23,6 +24,8 @@ namespace KP
                 Triangle[] arrayOutput = (Triangle[])jsonFormatter.ReadObject(fs); 
                 foreach (Triangle obj in arrayOutput)
                 {
+                    if (obj.A > 100000 || obj.B > 100000 || obj.C > 100000 || obj.Angle == Math.PI)
+                        throw new FileException("Файл содержит треугольник с недопустимыми параметрами.");
                     listBox_Triangles.Items.Add($"Треугольник №{++counter} ({obj.Type})");
                     picture.Add(obj);
                 }
